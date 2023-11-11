39a63dd5fbfa04ba6d0828c9c9ac435ab5aa6e12
hosshy84 <tatsuya.kaiden@gmail.com>
2023-11-11 14:14:28 +0900

    initial commit

---
+++ app/src/main/java/com/example/babymonitor/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.example.babymonitor
+
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.example.babymonitor.databinding.ActivityMainBinding
+import com.github.niqdev.mjpeg.DisplayMode
+import com.github.niqdev.mjpeg.Mjpeg
+import com.github.niqdev.mjpeg.MjpegView
+import java.time.LocalDate
+import java.time.Period
+import java.time.temporal.ChronoUnit
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    private val mjpegView : MjpegView by lazy {
+        findViewById(R.id.mjpeg_view)
+    }
+    private val streamURL = "http://192.168.68.18:8080/?action=stream"
+    private val timeout = 100
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.appBarMain.toolbar)
+
+        binding.appBarMain.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        initMenu()
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    private fun initMenu() {
+        val birthday = LocalDate.of(2023, 9, 18)
+        val today = LocalDate.now()
+        val period = Period.between(birthday, today)
+        val days = ChronoUnit.DAYS.between(birthday, today)
+        val age = findViewById<TextView>(R.id.textAge)
+        age.text = "${period.years}歳${period.months}か月${period.days}日(${days}日目)"
+    }
+}
---
+++ app/src/main/java/com/example/babymonitor/ui/home/HomeFragment.kt
@@ -0,0 +1,68 @@
+package com.example.babymonitor.ui.home
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.babymonitor.databinding.FragmentHomeBinding
+import com.github.niqdev.mjpeg.DisplayMode
+import com.github.niqdev.mjpeg.Mjpeg
+import com.github.niqdev.mjpeg.MjpegView
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+//        val textView: TextView = binding.textHome
+//        homeViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
+        return root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        loadIpCam(binding.mjpegView)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun loadIpCam(mjpegView: MjpegView) {
+        val streamURL = "http://192.168.68.18:8080/?action=stream"
+        val timeout = 100
+        Mjpeg.newInstance()
+            .open(streamURL, timeout)
+            .subscribe({
+                mjpegView.setSource(it)
+                mjpegView.setDisplayMode(DisplayMode.BEST_FIT)
+                mjpegView.showFps(true)
+            },
+                {
+                    Log.e("loadIpCam", it.toString())
+//                    Toast.makeText(this, "Error: $it", Toast.LENGTH_LONG).show()
+                })
+    }
+}

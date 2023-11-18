bf653672ce5d89b311a017a1cef99da1ee467a4f
hosshy84 <tatsuya.kaiden@gmail.com>
2023-11-19 00:26:48 +0900

    ExoPlayer追加

--- app/src/main/java/com/example/babymonitor/ui/live/LiveFragment.kt
+++ app/src/main/java/com/example/babymonitor/ui/live/LiveFragment.kt
@@ -1,12 +1,22 @@
 package com.example.babymonitor.ui.live
 
+import android.annotation.SuppressLint
+import android.os.Build
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.media3.common.MediaItem
+import androidx.media3.common.MimeTypes
+import androidx.media3.common.Player
+import androidx.media3.exoplayer.ExoPlayer
 import com.example.babymonitor.R
+import com.example.babymonitor.databinding.FragmentLiveBinding
 
 class LiveFragment : Fragment() {
 
@@ -16,12 +26,101 @@ class LiveFragment : Fragment() {
 
     private lateinit var viewModel: LiveViewModel
 
+    private var _binding: FragmentLiveBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val viewBinding get() = _binding!!
+
+//    private val viewBinding by lazy(LazyThreadSafetyMode.NONE) {
+//        FragmentLiveBinding.inflate(layoutInflater)
+//    }
+
+    private var player: Player? = null
+
+    private var playWhenReady = true
+    private var mediaItemIndex = 0
+    private var playbackPosition = 0L
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         viewModel = ViewModelProvider(this)[LiveViewModel::class.java]
-        return inflater.inflate(R.layout.fragment_live, container, false)
+        _binding = FragmentLiveBinding.inflate(inflater, container, false)
+        return viewBinding.root
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        if (Build.VERSION.SDK_INT > 23) {
+            initializePlayer()
+        }
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        hideSystemUi()
+        if (Build.VERSION.SDK_INT <= 23 || player == null) {
+            initializePlayer()
+        }
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        if (Build.VERSION.SDK_INT <= 23) {
+            releasePlayer()
+        }
+    }
+
+    public override fun onStop() {
+        super.onStop()
+        if (Build.VERSION.SDK_INT > 23) {
+            releasePlayer()
+        }
+    }
+
+    private fun initializePlayer() {
+        // ExoPlayer implements the Player interface
+        player = ExoPlayer.Builder(requireContext())
+            .build()
+            .also { exoPlayer ->
+                viewBinding.videoView.player = exoPlayer
+                // Update the track selection parameters to only pick standard definition tracks
+                exoPlayer.trackSelectionParameters = exoPlayer.trackSelectionParameters
+                    .buildUpon()
+                    .setMaxVideoSizeSd()
+                    .build()
+
+                val mediaItem = MediaItem.Builder()
+                    .setUri(getString(R.string.media_url_dash))
+                    .setMimeType(MimeTypes.APPLICATION_MPD)
+                    .build()
+                exoPlayer.setMediaItems(listOf(mediaItem), mediaItemIndex, playbackPosition)
+                exoPlayer.playWhenReady = playWhenReady
+                exoPlayer.prepare()
+            }
+    }
+
+    private fun releasePlayer() {
+        player?.let { player ->
+            playbackPosition = player.currentPosition
+            mediaItemIndex = player.currentMediaItemIndex
+            playWhenReady = player.playWhenReady
+            player.release()
+        }
+        player = null
+    }
+
+    @SuppressLint("InlinedApi")
+    private fun hideSystemUi() {
+        val window = requireActivity().window
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, viewBinding.videoView).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior =
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
     }
 
 }

309aa90c175088bf1fa55733c168e6fde014e3fe
hosshy84 <tatsuya.kaiden@gmail.com>
2023-12-16 23:54:48 +0900

    Firebaseを追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
 }
 
 buildscript {
@@ -9,6 +10,6 @@ buildscript {
         google()
     }
     dependencies {
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.5")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.6")
     }
 }

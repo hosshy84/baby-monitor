fa88280156ed52c44c0fc42b9422d0f736825812
hosshy84 <tatsuya.kaiden@gmail.com>
2024-02-06 21:24:42 +0900

    コメント追加

--- app/src/main/java/com/tatsuya/babymonitor/ui/zoom/ZoomGestureListener.kt
+++ app/src/main/java/com/tatsuya/babymonitor/ui/zoom/ZoomGestureListener.kt
@@ -5,6 +5,16 @@ import android.view.MotionEvent
 import android.view.View
 
 class ZoomGestureListener(private val view: View) : GestureDetector.SimpleOnGestureListener() {
+    /**
+     * タッチの検知開始イベント（何もしない）
+     */
+    override fun onDown(e: MotionEvent): Boolean {
+        return true
+    }
+
+    /**
+     * ダブルタップを検知して、倍率と位置をリセットする
+     */
     override fun onDoubleTap(e: MotionEvent): Boolean {
         view.scaleX = 1.0f
         view.scaleY = 1.0f
@@ -13,17 +23,17 @@ class ZoomGestureListener(private val view: View) : GestureDetector.SimpleOnGest
         return true
     }
 
-    override fun onDown(e: MotionEvent): Boolean {
-        return true
-    }
-
+    /**
+     * ドラッグ（スクロール）を検知して、表示位置を変更する
+     * ただし、倍率が等倍以下の場合は移動させない
+     */
     override fun onScroll(
         e1: MotionEvent?,
         e2: MotionEvent,
         distanceX: Float,
         distanceY: Float
     ): Boolean {
-        if (view.scaleX == 1.0f && view.scaleY == 1.0f) {
+        if (view.scaleX <= 1.0f && view.scaleY <= 1.0f) {
             return true
         }
 
--- app/src/main/java/com/tatsuya/babymonitor/ui/zoom/ZoomScaleGestureListener.kt
+++ app/src/main/java/com/tatsuya/babymonitor/ui/zoom/ZoomScaleGestureListener.kt
@@ -8,12 +8,20 @@ class ZoomScaleGestureListener(private val view: View) : ScaleGestureDetector.Si
     private var focusX = 0f
     private var focusY = 0f
 
+    /**
+     * スケールの変更を検知した時点で1度だけ発火
+     * 倍率変更時の基準となる位置を記憶
+     */
     override fun onScaleBegin(detector: ScaleGestureDetector): Boolean {
         focusX = detector.focusX
         focusY = detector.focusY
         return true
     }
 
+    /**
+     * スケールの変更が行われている間定期的に発火
+     * 倍率の指定と位置の固定（デフォルト値から変更しない）を行う
+     */
     override fun onScale(detector: ScaleGestureDetector): Boolean {
         scaleFactor *= detector.scaleFactor
         scaleFactor = 1.0f.coerceAtLeast(scaleFactor.coerceAtMost(5.0f))

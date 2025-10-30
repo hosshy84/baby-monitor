d7a223c4759d7cfea3f6c4ce712c2839f640521a
hosshy84 <tatsuya.kaiden@gmail.com>
2025-10-30 22:10:15 +0900

    不要な処理を削除

--- baby-monitor.android/app/src/main/java/com/tatsuya/babymonitor/MainActivity.kt
+++ baby-monitor.android/app/src/main/java/com/tatsuya/babymonitor/MainActivity.kt
@@ -39,7 +39,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_profile, R.id.nav_live
+                R.id.nav_live
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
